66dae3e7aeb3d524bd7983f72d94dbb1fe735d11
chenxp <chenxp@cn.ibm.com>
2019-09-16 16:29:16 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,22 @@
 # variables supplied from terraform.tfvars
 
-variable "iaas_username" {}
-variable "ibmcloud_iaas_api_key" {}
-variable "ibmcloud_api_key" {}
+variable "iaas_username" {
+  type = "string"
+  description = "sl user name"
+  default = "n/a"
+}
+
+variable "ibmcloud_iaas_api_key" {
+  type = "string"
+  description = "sl api key"
+  default = "n/a"
+}
+
+variable "ibmcloud_api_key" {
+  type = "string"
+  description = "ic api key"
+  default = "n/a"
+}
 
 provider "ibm" {
   softlayer_username = "${var.iaas_username}"
