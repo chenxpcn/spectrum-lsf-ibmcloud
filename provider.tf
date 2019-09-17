c67435b00714a5769d98e80e9d15ba2daddc8fc2
chenxp <chenxp@cn.ibm.com>
2019-09-17 13:21:14 +0800

    use os instead of image

--- provider.tf
+++ provider.tf
@@ -16,7 +16,8 @@ variable "ibmcloud_api_key" {
 }
 
 provider "ibm" {
+  version            = "~> 0.17"
   softlayer_username = "${var.iaas_username}"
   softlayer_api_key  = "${var.ibmcloud_iaas_api_key}"
-  ibmcloud_api_key    = "${var.ibmcloud_api_key}"
+  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
 }
--- sample.tf
+++ sample.tf
@@ -1,8 +1,8 @@
-resource "ibm_compute_vm_instance" "vm1" {
-  hostname             = "vm1"
-  domain               = "example.com"
-#  os_reference_code    = "CENTOS_7_64"
-  image_id             = 2459122
+resource "ibm_compute_vm_instance" "vm-master" {
+  hostname             = "vm-master"
+  domain               = "demo.cloud"
+  os_reference_code    = "REDHAT_7_64"
+#  image_id             = 2459122
   datacenter           = "dal13"
   network_speed        = 100
   hourly_billing       = true
