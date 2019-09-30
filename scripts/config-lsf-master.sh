c98260874c9fa5665827879d475a218414a56690
chenxp <chenxp@cn.ibm.com>
2019-09-30 16:35:16 +0800

    fix problems

--- scripts/config-lsf-master.sh
+++ scripts/config-lsf-master.sh
@@ -19,9 +19,7 @@ function LOG()
 
 LOG "Start config LSF master node ..."
 
-LOG "Deploy provioning.sh for resource connector"
-LOG "wget -nH -c --no-check-certificate -o $LOG_FILE -O /var/www/html/provisioning.sh $REMOTE_SCRIPT_PATH/provisioning.sh"
-wget -nv -nH -c --no-check-certificate -o $LOG_FILE -O /var/www/html/provisioning.sh $REMOTE_SCRIPT_PATH/provisioning.sh
+LOG "Set provioning.sh"
 sed -i `s/\[MASTER-IP-ADDRESS\]/'$MASTER_IP_ADDR'/` /var/www/html/provisioning.sh
 
 LOG "Modify $LSF_ENVDIR/lsf.cluster.$CLUSTER_NAME"
@@ -31,25 +29,26 @@ LOG "Modify $LSF_ENVDIR/resource_connector/softlayer/conf/credentials"
 sed -i 's/^softlayer_access_user_name =.\+/softlayer_access_user_name = '$SL_USER'/; s/softlayer_secret_api_key =.\+/softlayer_secret_api_key = '$SL_APIKEY'/' $LSF_ENVDIR/resource_connector/softlayer/conf/credentials
 
 LOG "Modify $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_config.json"
-NEW_VALUE=`echo "$LSF_ENVDIR"|sed 's#\/#\\\/#g'`
-sed -i 's/\( \+"SOFTLAYER_CREDENTIAL_FILE": "\).\+\,/\1'$NEW_VALUE'\/resource_connector\/softlayer\/conf\/credentials"\,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_config.json
+NEW_VALUE=`echo "$LSF_ENVDIR/resource_connector/softlayer/conf/credentials"|sed 's#\/#\\\/#g'`
+sed -i 's/\( \+"SOFTLAYER_CREDENTIAL_FILE": "\).\+\,/\1'$NEW_VALUE'"\,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_config.json
 
 LOG "Modify $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json"
-sed -i 's/"maxNumber": [0-9]\+,/"maxNumber": 10,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
-sed -i 's/"ncpus": \["Numeric", ".\+"/"ncpus": \["Numeric", "'$SLAVE_CORES'"/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
-sed -i 's/"mem": \["Numeric", ".\+"/"mem": \["Numeric", "'$SLAVE_MEMORY'"/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
+sed -i 's/"maxNumber": [0-9]\+\,/"maxNumber": 10\,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
+sed -i 's/"ncpus": \["Numeric"\, ".\+"/"ncpus": \["Numeric"\, "'$SLAVE_CORES'"/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
+sed -i 's/"mem": \["Numeric"\, ".\+"/"mem": \["Numeric"\, "'$SLAVE_MEMORY'"/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
 sed -i 's/"softlayercomp"/"softlayerhost"/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
 sed -i 's/"imageId": ".\+",/"imageId": "'$IMAGE_NAME'",/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
-sed -i 's/"datacenter": ".\+",/"datacenter": "'$DATA_CENTER'",/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
-sed -i 's/"vlanNumber": ".\+",/"vlanNumber": "'$PRIVATE_VLAN_NUMBER'",/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
-sed -i 's/"privateNetworkOnlyFlag": false,/"privateNetworkOnlyFlag": true,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
-sed -i 's/"postProvisionURL": ".\+",/"postProvisionURL": "http:\/\/'$MASTER_IP_ADDR'\/provisioning.sh",/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
+sed -i 's/"datacenter": ".\+"\,/"datacenter": "'$DATA_CENTER'"\,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
+sed -i 's/"vlanNumber": ".\+"\,/"vlanNumber": "'$PRIVATE_VLAN_NUMBER'"\,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
+sed -i 's/"privateNetworkOnlyFlag": false\,/"privateNetworkOnlyFlag": true\,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
+sed -i 's/"postProvisionURL": ".\+"\,/"postProvisionURL": "http:\/\/'$MASTER_IP_ADDR'\/provisioning.sh"\,/' $LSF_ENVDIR/resource_connector/softlayer/conf/softlayerprov_templates.json
 
 LOG "Modify $LSF_ENVDIR/lsbatch/$CLUSTER_NAME/configdir/lsb.modules"
 sed -i 's/#schmod_demand/schmod_demand/' $LSF_ENVDIR/lsbatch/$CLUSTER_NAME/configdir/lsb.modules
 
 LOG "Modify $LSF_ENVDIR/lsbatch/$CLUSTER_NAME/configdir/lsb.queues"
-sed -i '/QUEUE_NAME \+= normal/a\RC_HOSTS        = softlayerhost; /RC_HOSTS        = softlayerhost/a\RC_ACCOUNT  = lsf-demo-dynamic-host' $LSF_ENVDIR/lsbatch/$CLUSTER_NAME/configdir/lsb.queues
+sed -i '/QUEUE_NAME \+= normal/a\RC_HOSTS = softlayerhost' $LSF_ENVDIR/lsbatch/$CLUSTER_NAME/configdir/lsb.queues
+sed -i '/RC_HOSTS = softlayerhost/a\RC_ACCOUNT = lsf-demo-dynamic-host' $LSF_ENVDIR/lsbatch/$CLUSTER_NAME/configdir/lsb.queues
 
 LOG "Modify $LSF_ENVDIR/lsf.conf"
 echo LSB_RC_EXTERNAL_HOST_FLAG=\"softlayerhost\">>$LSF_ENVDIR/lsf.conf
@@ -62,12 +61,14 @@ LOG "Modify $LSF_ENVDIR/lsf.shared"
 sed -i 's/#\( \+\)softlayerhost/ \1softlayerhost/' $LSF_ENVDIR/lsf.shared
 
 LOG "Disable master node as compute node"
-badmin hclose lsf-master.hpc.ibmcloud>>$LOG_FILE
+badmin hclose lsf-master>>$LOG_FILE
 
 LOG "Restart the LSF daemons"
-lsadmin limrestart>>$LOG_FILE
-lsadmin resrestart>>$LOG_FILE
-badmin mbdrestart>>$LOG_FILE
+echo y>/root/installer/all_yes
+echo y>>/root/installer/all_yes
+lsadmin limrestart</root/installer/all_yes>>$LOG_FILE
+lsadmin resrestart</root/installer/all_yes>>$LOG_FILE
+badmin mbdrestart</root/installer/all_yes>>$LOG_FILE
 
 
 LOG "Complete config LSF master node."
