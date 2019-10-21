8b923522f2a605c5c6e14d0ad93556cb7d7c3fc0
chenxp <chenxp@cn.ibm.com>
2019-10-21 17:21:44 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "ibm" {
   version            = "~> 0.17"
-  softlayer_username = "${var.iaas_username}"
-  softlayer_api_key  = "${var.ibmcloud_iaas_api_key}"
+  iaas_classic_username = "${var.iaas_username}"
+  iaas_classic_api_key  = "${var.ibmcloud_iaas_api_key}"
   ibmcloud_api_key   = "${var.ibmcloud_api_key}"
 }
